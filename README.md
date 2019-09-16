19e0c9e41fae93192f66cebdb8cec7f3d728e173
eonnine <eonnine@github.com>
2019-09-16 15:23:38 +0900

    create load, setLoadParam, onLoad features

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ Promise 방식 삭제, Create, Clear, Destory 등 Hook 삭제, 코드 간소화
 ```javascript
 /**
  Dialog로 호출한 페이지에서는 Message 객체와 Dialog 객체를 사용할 수 있습니다.
- Message 객체는 JS Object 아래와 같이 구성되어있습니다.
+ @Message 객체는 JS Object 아래와 같이 구성되어있습니다.
  
  Message = {
    on: function // 메세지 리스너 함수
@@ -32,8 +32,8 @@ var dialog = new Dialog('불러올 jsp url');
 dialog.emit('sample', 'parameter'); //  console => 'This is Sample Message', 'parameter'
 
 
+@Dialog 객체는 아래와 같이 구성되어 있습니다.
 /**
-*  Dialog 객체는 아래와 같이 구성되어 있습니다.
 *  load 사용법
 */
 
@@ -51,19 +51,20 @@ new Dialog('불러올 jsp url')
 });
 
 
----  
+---
+
 ***2019-07-23***  
--최초 커밋  
--create, destroy, render, clear, onMessage, postMessage, dialogConstructor, renderConstructor 기능 생성  
+-최초 커밋
+-create, destroy, render, clear, onMessage, postMessage, dialogConstructor, renderConstructor 기능 생성
 
----  
+---
 ***2019-07-24***  
--메모리 누수 제거  
+-메모리 누수 제거
 
----  
-***2019-08-30***  
--Promise 방식 제거, Message 기능을 제외한 모든 기능 삭제  
+---
+***2019-08-30***
+-Promise 방식 제거, Message 기능을 제외한 모든 기능 삭제
 
 ---  
 ***2019-09-16***  
--load 기능 추가.  
+-LazyLoad 기능 추가.
--- dialog.js
+++ dialog.js
@@ -1,3 +1,9 @@
+/**
+ * *******
+ * Dialog *
+ * *******
+ * Developed by Eg 2019-07-23
+ */
 (function (factory) {
 	if(typeof define === 'function' && define.amd ){
 		define(function () {
@@ -8,7 +14,6 @@
 	}
 }(function () { 'use strict'
 	var
-	__UrlPostFix = '.dtnc',
 	/*
 	 * 스크립트 태그(<script></script>)만 선택하는 정규식
 	 */ 
@@ -22,14 +27,19 @@
 	 */ 
 	__REGEXP_Annotaion = /(\/\*(\s|\S)*?\*\/)|<!-{2,}(\s|\S)*?-{2,}>|^\/\/.*|(\/\/.*)/g,
 	__HasProp = Object.prototype.hasOwnProperty,
-	/**
-	 * ajax로 파일을 로드할 때 로드된 문자열을 이 객체에 저장힙니다.
-	 * 그 후 동일한 파일의 요청이 들어오면 ajax요청을 다시 보내지 않고 이 객체에서 꺼내서 할당합니다.
+	
+	/*
+	 * Ajax요청으로 파일을 로드할 때 로드된 문자열을 이 객체에 저장힙니다.
+	 * 그 후 동일한 파일의 요청이 들어오면 Ajax요청을 다시 보내지 않고 이 객체에서 꺼내서 할당합니다.
 	 * 이 객체는 hasOwnProperty만 사용하기 때문에 Object의 prototype을 상속받지 않고 __HasProp으로 대신 사용합니다.
 	 */
 	__Cache = Object.create(null);
 
 	function Dialog (url) {
+  	if( !(this instanceof Dialog) ){
+  		throw new SyntaxError('[Dialog] "new" constructor operator is required');
+  	}
+  	
 		this.validator(url);
 		this.initProps(url);
 		this.create();
@@ -37,7 +47,30 @@
 	}
 	
 	Dialog.prototype.initProps = function (url) {
-		this.url = url + __UrlPostFix;
+		this.url = url;
+		this.loadParam = null;
+		/**
+		 * 호출한 Dialog객체의 load함수가 종료된 후 실행될 콜백 함수입니다.
+		 */
+		this.loadListener = null;
+		/**
+		 * 파라미터로 받은 함수의 리턴값을 loadListener의 인자로 하여 실행합니다.
+		 */
+		this.load = function (fn) {
+			var result = fn.call(this.scope, this.loadParam);
+			if( this.loadListener ){
+				this.loadListener.call(this.callee, result);
+			}
+		}.bind(this);
+		/**
+		 * Dialog객체 내부의 호스트 객체에 thisBinding할 Dialog 객체
+		 */
+		this.dialog = {
+			load: this.load
+		};
+		/**
+		 * Dialog객체 내부의 호스트 객체에 thisBinding할 메세지 객체
+		 */
 		this.message = {};
 		/**
 		 * Dialog로 호출한 페이지와 Message.on함수의 스코프 영역으로 사용할 객체입니다.
@@ -50,7 +83,9 @@
 		 * Dialog 객체 생성 시 반한해줄 객체를 생성합니다.
 		 */
 		this.callee = {
-			emit: this.createEmitFn()
+			emit: this.createEmitFn(),
+			onLoad: this.onLoad.bind(this),
+			setLoadParam: this.setLoadParam.bind(this)
 		};
 		
 		// scope객체의 property 변경이나 재할당을 하지 못하게 처리합니다.
@@ -60,13 +95,11 @@
 			writable: false
 		};
 		Object.defineProperties(this.scope, { 'on': descriptor, 'emit': descriptor });
+		Object.defineProperties(this.dialog, { 'load': descriptor });
 	}
 	
 	// Dialog 객체 생성 시 필수 요소 체크
 	Dialog.prototype.validator = function (url) {
-  	if( !(this instanceof Dialog) ){
-  		this.throwError('syntax', '"new" constructor operator is required');
-  	}
 		if(url == null){
 			this.throwError('syntax', 'option\'s prop is required: "url"');
 		}
@@ -79,8 +112,8 @@
 		var _this = this;
 		/**
 		 * url에 맞는 페이지를 호출합니다.
-		 * @param strLoadedModule 불러온 페이지의 문자열 
-		 */
+		 * strLoadedModule: 불러온 페이지의 문자열 
+		 **/
 		this.getDialog(this.url, function (strLoadedModule) {
 			// 페이지 문자열에서 주석을 제거합니다.
 			var 	strDOM = strLoadedModule.replace(__REGEXP_Annotaion, '');
@@ -94,7 +127,7 @@
 			
 			// 스크립트 문자열들을 루프돌리면서 함수로 변환한 뒤 Message 객체를 스코프 영역으로 설정합니다.
 			strSCRIPTNodes.forEach(function (v, i) {
-				_this.makeFn(v.replace(__REGEXP_ScriptTags, ''))({ on: _this.createOnFn(), emit: _this.createEmitFn() });
+				_this.makeFn(v.replace(__REGEXP_ScriptTags, ''))(_this.scope, _this.dialog);
 			});
 			
 		});
@@ -106,7 +139,7 @@
 	 * 최초 로딩 이후에 재로딩되는 로직은 피하는걸 권장합니다.
 	 */  
 	Dialog.prototype.makeFn = function (script) {
-		return Function('Message', script);
+		return Function('Message', 'Dialog', script);
 	}
 	
 	/**
@@ -129,6 +162,16 @@
 				this.throwError('error', 'not found message: ' + key);
 			}
 		}.bind(this);
+	}
+	
+	Dialog.prototype.setLoadParam = function (param) {
+		this.loadParam = param;
+		return this.callee;
+	}
+	
+	Dialog.prototype.onLoad = function (fn) {
+		this.loadListener = fn;
+		return this.callee;
 	}
 
 	/**
